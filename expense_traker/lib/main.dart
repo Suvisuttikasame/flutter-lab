d23030925b6ff4a4861ff35d0f41ba4f2303855b
Nuttayos Suvisuttikasame <nuttayossuvisuttikasame@MacBook-Pro-khxngNuttayos.local>
2024-04-28 18:40:34 +0700

    - [feat] setup theme

--- expense_traker/lib/main.dart
+++ expense_traker/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:expense_traker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+final kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 179, 151, 227));
+
 void main() {
   runApp(const MyApp());
 }
@@ -14,23 +17,55 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // TRY THIS: Try running your application with "flutter run". You'll see
-          // the application has a purple toolbar. Then, without quitting the app,
-          // try changing the seedColor in the colorScheme below to Colors.green
-          // and then invoke "hot reload" (save your changes or press the "hot
-          // reload" button in a Flutter-supported IDE, or press "r" if you used
-          // the command line to start the app).
-          //
-          // Notice that the counter didn't reset back to zero; the application
-          // state is not lost during the reload. To reset the state, use hot
-          // restart instead.
-          //
-          // This works for code too, not just values: Most code changes can be
-          // tested with just a hot reload.
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
+                // This is the theme of your application.
+                //
+                // TRY THIS: Try running your application with "flutter run". You'll see
+                // the application has a purple toolbar. Then, without quitting the app,
+                // try changing the seedColor in the colorScheme below to Colors.green
+                // and then invoke "hot reload" (save your changes or press the "hot
+                // reload" button in a Flutter-supported IDE, or press "r" if you used
+                // the command line to start the app).
+                //
+                // Notice that the counter didn't reset back to zero; the application
+                // state is not lost during the reload. To reset the state, use hot
+                // restart instead.
+                //
+                // This works for code too, not just values: Most code changes can be
+                // tested with just a hot reload.
+                // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                // useMaterial3: true,
+                )
+            .copyWith(
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+          ),
+          cardTheme: const CardTheme().copyWith(
+            color: kColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: kColorScheme.primaryContainer),
+          ),
+          textTheme: const TextTheme().copyWith(
+            titleLarge: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+            titleMedium: const TextStyle(
+              color: Colors.black,
+            ),
+            bodyLarge: const TextStyle(
+              color: Colors.black,
+            ),
+            bodyMedium: const TextStyle(
+              fontSize: 14,
+              color: Colors.black,
+            ),
+          ),
         ),
         home: const Expenses());
   }
