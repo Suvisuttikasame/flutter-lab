3de9e241cee9fe2564a61fd3ccb060a1f028f250
Nuttayos Suvisuttikasame <nuttayossuvisuttikasame@MacBook-Pro-khxngNuttayos.local>
2024-04-28 09:14:18 +0700

    - [feat] expense & expense list

---
+++ expense_traker/lib/expenses.dart
@@ -0,0 +1,33 @@
+import 'package:expense_traker/expenses_list.dart';
+import 'package:expense_traker/model/expense.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpense = [
+    Expense(
+      title: 'Flutter',
+      amount: 10.99,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text('The chart'),
+          Expanded(child: ExpensesList(expenses: _registeredExpense)),
+        ],
+      ),
+    );
+  }
+}
