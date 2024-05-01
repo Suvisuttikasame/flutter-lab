d1f041bedbbd43a5e7a1710603d4d7e69f4e0241
Nuttayos Suvisuttikasame <nuttayossuvisuttikasame@MacBook-Pro-khxngNuttayos.local>
2024-05-01 20:20:08 +0700

    - [feat] meal detail screen

--- meal_app/lib/widgets/meal_item.dart
+++ meal_app/lib/widgets/meal_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meal_app/model/meal.dart';
+import 'package:meal_app/screens/meal_detail_screen.dart';
 import 'package:meal_app/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
@@ -16,72 +17,83 @@ class MealItem extends StatelessWidget {
     return meal.affordability.name;
   }
 
+  void selectMeal(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (ctx) => MealDetailScreen(meal: meal)),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(20),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-      clipBehavior: Clip.hardEdge,
-      child: Stack(
-        children: [
-          FadeInImage(
-            placeholder: MemoryImage(kTransparentImage),
-            image: NetworkImage(
-              meal.imageUrl,
+    return InkWell(
+      onTap: () {
+        selectMeal(context);
+      },
+      child: Card(
+        margin: const EdgeInsets.all(20),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        clipBehavior: Clip.hardEdge,
+        child: Stack(
+          children: [
+            FadeInImage(
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(
+                meal.imageUrl,
+              ),
+              fit: BoxFit.cover,
+              height: 200,
+              width: double.infinity,
             ),
-            fit: BoxFit.cover,
-            height: 200,
-            width: double.infinity,
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              padding: const EdgeInsets.all(16),
-              color: Colors.black54,
-              child: Column(
-                children: [
-                  Text(
-                    meal.title,
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                color: Colors.black54,
+                child: Column(
+                  children: [
+                    Text(
+                      meal.title,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
                     ),
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      MealItemTrait(
-                        icon: Icons.calendar_month,
-                        label: '${meal.duration} min',
-                      ),
-                      const SizedBox(
-                        width: 8,
-                      ),
-                      MealItemTrait(
-                        icon: Icons.handyman,
-                        label: complexityToText,
-                      ),
-                      const SizedBox(
-                        width: 8,
-                      ),
-                      MealItemTrait(
-                        icon: Icons.grade,
-                        label: affordToText,
-                      ),
-                    ],
-                  )
-                ],
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        MealItemTrait(
+                          icon: Icons.calendar_month,
+                          label: '${meal.duration} min',
+                        ),
+                        const SizedBox(
+                          width: 8,
+                        ),
+                        MealItemTrait(
+                          icon: Icons.handyman,
+                          label: complexityToText,
+                        ),
+                        const SizedBox(
+                          width: 8,
+                        ),
+                        MealItemTrait(
+                          icon: Icons.grade,
+                          label: affordToText,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
